3f7b75b5e005ebc7d3c104e846ee13ec3a6c5ca1
ICyeborg <viktor_gies@gmx.de>
2024-08-22 23:17:38 +0200

    [pixel-maker-4] creating tile data

--- game/editor.py
+++ game/editor.py
@@ -11,6 +11,7 @@ class Editor:
     def __init__(self):
         # main setup
         self.display_surface = pygame.display.get_surface()
+        self.canvas_data = {}
 
         # navigation
         self.origin = vector()
@@ -24,9 +25,37 @@ class Editor:
 
         # selection
         self.selection_index = 2
+        self.last_selected_cell = None
 
         # menu
         self.menu = Menu()
+
+    def get_current_cell(self):
+        distance_to_orogin = vector(mouse_pos()) - self.origin
+
+        if distance_to_orogin.x > 0:
+            col = int(distance_to_orogin.x / TILE_SIZE)
+        else:
+            col = int(distance_to_orogin.x / TILE_SIZE) - 1
+
+        if distance_to_orogin.y > 0:
+            row = int(distance_to_orogin.y / TILE_SIZE)
+        else:
+            row = int(distance_to_orogin.y / TILE_SIZE) - 1
+
+        return col, row
+
+    def canvas_add(self):
+        if mouse_buttons()[0] and not self.menu.rect.collidepoint(mouse_pos()):
+            current_cell = self.get_current_cell()
+
+            if current_cell != self.last_selected_cell:
+
+                if current_cell in self.canvas_data:
+                    self.canvas_data[current_cell].add_id(self.selection_index)
+                else:
+                    self.canvas_data[current_cell] = CancasTile(self.selection_index)
+                self.last_selected_cell = current_cell
 
     # input
     def event_loop(self):
@@ -37,6 +66,7 @@ class Editor:
             self.pan_input(event)
             self.select_hotkeys(event)
             self.menu_click(event)
+            self.canvas_add()
 
     def pan_input(self, event):
         # middle mouse button pressed / released
@@ -64,7 +94,7 @@ class Editor:
                 self.selection_index += 1
             if event.key == pygame.K_LEFT:
                 self.selection_index -= 1
-        self.selection_index = max(2,(min(self.selection_index, 18)))
+        self.selection_index = max(2, (min(self.selection_index, 18)))
 
     # drawing
     def draw_tile_lines(self):
@@ -84,7 +114,7 @@ class Editor:
             y = offset_vector.y + row * TILE_SIZE
             pygame.draw.line(self.support_line_surface, LINE_COLOUR, (0, y), (WINDOW_WIDTH, y))
 
-        self.display_surface.blit(self.support_line_surface,(0,0))
+        self.display_surface.blit(self.support_line_surface, (0, 0))
 
     def menu_click(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN and self.menu.rect.collidepoint(mouse_pos()):
@@ -99,3 +129,33 @@ class Editor:
         self.draw_tile_lines()
         pygame.draw.circle(self.display_surface, 'red', self.origin, 10)
         self.menu.display(self.selection_index)
+
+
+class CancasTile():
+    def __init__(self, tile_id):
+        # terrain
+        self._has_terrain = False
+        self.terrain_neighbors = []
+
+        # water
+        self.has_water = False
+        self.water_on_top = False
+
+        # coin
+        self.coin = None
+
+        # enemy
+        self.enemy = None
+
+        # objects
+        self.objects = []
+
+        self.add_id(tile_id)
+
+    def add_id(self, tile_id):
+        options = {key: value['style'] for key, value in EDITOR_DATA.items()}
+        match options[tile_id]:
+            case 'terrain': self._has_terrain = True
+            case 'water': self.has_water = True
+            case 'coin': self.coin = tile_id
+            case 'enemy': self.enemy = tile_id
