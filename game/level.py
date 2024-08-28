f91cee5755894876c99827d7968ecb97d7d1fe40
ICyeborg <viktor_gies@gmx.de>
2024-08-28 23:57:05 +0200

    [pixel-maker-17] take over tiles and player from editor to level

--- game/level.py
+++ game/level.py
@@ -1,16 +1,33 @@
 import sys
-from pygame.math import Vector2 as vector
 from settings import *
 from support import *
+from game.sprites import Generic, Player
 
 
 class Level:
-    def __init__(self, grid, switch):
+    def __init__(self, grid, switch, asset_dict):
         self.display_surface = pygame.display.get_surface()
         self.switch = switch
 
+        # groups
+        self.all_sprites = pygame.sprite.Group()
+
+        self.build_level(grid, asset_dict)
+
+    def build_level(self, grid, asset_dict):
+        for layer_name, layer in grid.items():
+            for pos, data in layer.items():
+                if layer_name == 'terrain':
+                    Generic(pos, asset_dict['land'][data], self.all_sprites)
+
+                match data:
+                    case 0: self.player = Player(pos, self.all_sprites)
+
     def run(self, dt):
-        self.display_surface.fill('red')
+        self.event_loop()
+        self.display_surface.fill(SKY_COLOUR)
+        self.all_sprites.update(dt)
+        self.all_sprites.draw(self.display_surface)
 
     def event_loop(self):
         for event in pygame.event.get():
