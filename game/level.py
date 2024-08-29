85fea190fb345d5d1c52eb128295d81c497fc16e
ICyeborg <viktor_gies@gmx.de>
2024-08-30 00:03:00 +0200

    [pixel-maker-18] taken over animated tiles and coins from editor to level

--- game/level.py
+++ game/level.py
@@ -1,7 +1,7 @@
 import sys
 from settings import *
 from support import *
-from game.sprites import Generic, Player
+from game.sprites import Generic, Animated, Particle, Coin, Player
 
 
 class Level:
@@ -11,22 +11,40 @@ class Level:
 
         # groups
         self.all_sprites = pygame.sprite.Group()
+        self.coin_sprites = pygame.sprite.Group()
 
         self.build_level(grid, asset_dict)
+
+        self.particle_surfs = asset_dict['particle']
 
     def build_level(self, grid, asset_dict):
         for layer_name, layer in grid.items():
             for pos, data in layer.items():
                 if layer_name == 'terrain':
                     Generic(pos, asset_dict['land'][data], self.all_sprites)
+                if layer_name == 'water':
+                    if data == 'top':
+                        Animated(pos, asset_dict['water top'], self.all_sprites)
+                    else:
+                        Generic(pos, asset_dict['water bottom'], self.all_sprites)
 
                 match data:
                     case 0: self.player = Player(pos, self.all_sprites)
+                    case 4: Coin('gold', pos, asset_dict['gold'], [self.all_sprites, self.coin_sprites])
+                    case 5: Coin('silver', pos, asset_dict['silver'], [self.all_sprites, self.coin_sprites])
+                    case 6: Coin('diamond', pos, asset_dict['diamond'], [self.all_sprites, self.coin_sprites])
+
+    def get_coins(self):
+        collided_coins = pygame.sprite.spritecollide(self.player, self.coin_sprites, True)
+        for sprite in collided_coins:
+            Particle(sprite.rect.center, self.particle_surfs, self.all_sprites)
 
     def run(self, dt):
         self.event_loop()
-        self.display_surface.fill(SKY_COLOUR)
         self.all_sprites.update(dt)
+        self.get_coins()
+
+        self.display_surface.fill(SKY_COLOUR)
         self.all_sprites.draw(self.display_surface)
 
     def event_loop(self):
--- game/sprites.py
+++ game/sprites.py
@@ -1,5 +1,6 @@
 import pygame.sprite
 from pygame.math import Vector2 as vector
+from settings import *
 
 
 class Generic(pygame.sprite.Sprite):
@@ -7,6 +8,41 @@ class Generic(pygame.sprite.Sprite):
         super().__init__(group)
         self.image = surf
         self.rect = self.image.get_rect(topleft=pos)
+
+
+class Animated(Generic):
+    def __init__(self, pos, assets, group):
+        self.animation_frames = assets
+        self.frame_index = 0
+        super().__init__(pos, self.animation_frames[self.frame_index], group)
+
+    def animate(self, dt):
+        self.frame_index += ANIMATION_SPEED * dt
+        self.frame_index = 0 if self.frame_index >= len(self.animation_frames) else self.frame_index
+        self.image = self.animation_frames[int(self.frame_index)]
+
+    def update(self, dt):
+        self.animate(dt)
+
+
+class Particle(Animated):
+    def __init__(self, pos, assets, group):
+        super().__init__(pos, assets, group)
+        self.rect = self.image.get_rect(center=pos)
+
+    def animate(self, dt):
+        self.frame_index += ANIMATION_SPEED * dt
+        if self.frame_index < len(self.animation_frames):
+            self.image = self.animation_frames[int(self.frame_index)]
+        else:
+            self.kill()
+
+
+class Coin(Animated):
+    def __init__(self, coin_type, pos, assets, group):
+        super().__init__(pos, assets, group)
+        self.rect = self.image.get_rect(center=pos)
+        self.coin_type = coin_type
 
 
 class Player(Generic):
